5ec67cb8009aea00b2d1335e907ac1b83851b730
FlutLab.io <>
2022-03-15 09:25:45 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -49,191 +49,193 @@ class _MyHomePageState extends State<MyHomePage> {
         // The title text which will be shown on the action bar
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              'Add Employee Record',
-              style: TextStyle(
-                fontSize: 36,
-                color: Colors.grey,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _fullNameController,
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.grey,
-                fontWeight: FontWeight.w600,
-              ),
-              decoration: InputDecoration(
-                focusColor: Colors.white,
-                //add prefix icon
-                prefixIcon: Icon(
-                  Icons.person_outline_rounded,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                'Add Employee Record',
+                style: TextStyle(
+                  fontSize: 36,
                   color: Colors.grey,
-                ),
-
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.blue, width: 1.0),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                fillColor: Colors.grey,
-
-                hintText: "Enter FullName",
-
-                //make hint text
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-
-                //create lable
-                labelText: 'Full Name',
-                //lable style
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _companyController,
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.grey,
-                fontWeight: FontWeight.w600,
-              ),
-              decoration: InputDecoration(
-                focusColor: Colors.white,
-                //add prefix icon
-                prefixIcon: Icon(
-                  Icons.person_outline_rounded,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _fullNameController,
+                style: TextStyle(
+                  fontSize: 24,
                   color: Colors.grey,
+                  fontWeight: FontWeight.w600,
                 ),
-
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.blue, width: 1.0),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                fillColor: Colors.grey,
-
-                hintText: "Enter Company Name",
-
-                //make hint text
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-
-                //create lable
-                labelText: 'Company',
-                //lable style
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _ageController,
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.grey,
-                fontWeight: FontWeight.w600,
-              ),
-              decoration: InputDecoration(
-                focusColor: Colors.white,
-                //add prefix icon
-                prefixIcon: Icon(
-                  Icons.person_outline_rounded,
-                  color: Colors.grey,
-                ),
-
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.blue, width: 1.0),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                fillColor: Colors.grey,
-
-                hintText: "Enter your Age",
-
-                //make hint text
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-
-                //create lable
-                labelText: 'Age',
-                //lable style
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          isProcessing
-              ? CircularProgressIndicator()
-              : Container(
-                  width: double.maxFinite,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 20.0,
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
                   ),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      var user = UserModel(fullName: _fullNameController.text, age: int.parse(_ageController.text), company: _companyController.text);
-                      setState(() {
-                        isProcessing = true;
-                      });
-                      await userDb.addUser(user);
-                      isProcessing = false;
-                    },
-                    child: Text(
-                      'SAVE',
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Enter FullName",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Full Name',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _companyController,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Enter Company Name",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Company',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _ageController,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Enter your Age",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Age',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            isProcessing
+                ? CircularProgressIndicator()
+                : Container(
+                    width: double.maxFinite,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        var user = UserModel(fullName: _fullNameController.text, age: int.parse(_ageController.text), company: _companyController.text);
+                        setState(() {
+                          isProcessing = true;
+                        });
+                        await userDb.addUser(user);
+                        isProcessing = false;
+                      },
+                      child: Text(
+                        'SAVE',
+                      ),
                     ),
                   ),
-                ),
-        ],
+          ],
+        ),
       ),
     );
   }
