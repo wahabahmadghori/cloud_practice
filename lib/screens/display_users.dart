38f2868ba423b09faac01b9fd14fc8c49a38bdd3
FlutLab.io <>
2022-03-15 10:36:26 +0000

    Autosave

--- lib/screens/display_users.dart
+++ lib/screens/display_users.dart
@@ -6,24 +6,26 @@ class DisplayUsers extends StatelessWidget {
   Widget build(BuildContext context) {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
 
-    return FutureBuilder<DocumentSnapshot>(
-      future: users.doc().get(),
-      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
+    return Scaffold(
+      body: FutureBuilder<DocumentSnapshot>(
+        future: users.doc().get(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
 
-        if (snapshot.hasData && !snapshot.data!.exists) {
-          return Text("Document does not exist");
-        }
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return Text("Document does not exist");
+          }
 
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-          return Text("Full Name: ${data['fullName']} ${data['company']}");
-        }
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+            return Text("Full Name: ${data['fullName']} ${data['company']}");
+          }
 
-        return Text("loading");
-      },
+          return Text("loading");
+        },
+      ),
     );
   }
 }
