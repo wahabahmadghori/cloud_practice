44af63875fa994cc53080f9af773d2ed0613f05c
FlutLab.io <>
2022-03-16 06:32:20 +0000

    Autosave

--- lib/screens/add_question.dart
+++ lib/screens/add_question.dart
@@ -17,6 +17,7 @@ class _AddQuestionState extends State<AddQuestion> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'Add New Question',
@@ -29,6 +30,7 @@ class _AddQuestionState extends State<AddQuestion> {
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
                   controller: _questionController,
+                  minLines: 3,
                   style: TextStyle(
                     fontSize: 24,
                     color: Colors.grey,
@@ -67,6 +69,96 @@ class _AddQuestionState extends State<AddQuestion> {
                     ),
                   ),
                 ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _optionAController,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      decoration: InputDecoration(
+                        focusColor: Colors.white,
+                        //add prefix icon
+                        prefixIcon: Icon(
+                          Icons.person_outline_rounded,
+                        ),
+
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.blue, width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+
+                        hintText: "Type OptionA...",
+
+                        //make hint text
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                        //create lable
+                        labelText: 'A',
+                        //lable style
+                        labelStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _optionBController,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      decoration: InputDecoration(
+                        focusColor: Colors.white,
+                        //add prefix icon
+                        prefixIcon: Icon(
+                          Icons.person_outline_rounded,
+                        ),
+
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.blue, width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+
+                        hintText: "Type OptionB...",
+
+                        //make hint text
+                        hintStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                        //create lable
+                        labelText: 'OptionB',
+                        //lable style
+                        labelStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
