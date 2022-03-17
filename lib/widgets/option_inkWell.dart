0ec7626e83a88643518dce78544eaff84821bdae
FlutLab.io <>
2022-03-17 05:59:00 +0000

    Autosave

--- lib/widgets/option_inkWell.dart
+++ lib/widgets/option_inkWell.dart
@@ -20,7 +20,7 @@ class _OptionInkWellState extends State<OptionInkWell> {
           });
         } else {
           setState(() {
-            _color = Colors.pinkAccent;
+            _color = Colors.pink[100]!;
           });
         }
       },
