4aaddd82d633d72a621100e28bc3827042877525
FlutLab.io <>
2022-03-17 05:43:24 +0000

    Autosave

--- lib/widgets/option_inkWell.dart
+++ lib/widgets/option_inkWell.dart
@@ -10,40 +10,25 @@ class OptionInkWell extends StatefulWidget {
 class _OptionInkWellState extends State<OptionInkWell> {
   @override
   Widget build(BuildContext context) {
-    Color optionColor = Colors.white;
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.all(8),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: optionColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
-            side: BorderSide(color: Colors.red),
-          ),
-        ),
-        onPressed: () {
-          print('Hello.....................');
+    Color _color = Colors.white;
+    return InkWell(
+      onTap: () {
+        if (widget.option == widget.answer) {
           setState(() {
-            optionColor = Colors.greenAccent;
+            _color = Colors.greenAccent;
           });
-          if (widget.option == widget.answer) {
-            setState(() {
-              optionColor = Colors.greenAccent;
-            });
-          } else {
-            setState(() {
-              optionColor = Colors.pinkAccent;
-            });
-          }
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            widget.option,
-            style: TextStyle(color: Colors.black54),
-          ),
-        ),
+        } else {
+          setState(() {
+            _color = Colors.greenAccent;
+          });
+        }
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: _color),
+        child: Text(widget.option),
       ),
     );
   }
